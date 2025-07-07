343d30a3d1a9eeeb86d6010b2d625382371fbf0b
sebastiansemua <sebastiansemua@hotmail.com>
2025-07-07 08:20:35 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Start VNC server
 sudo -u user vncserver :1 -geometry 1280x800 -depth 24
+
+# Start noVNC proxy
 /usr/share/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 8080
